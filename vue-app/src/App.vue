2e7655764c3701b429076e46abb23c78008ced55
ruhiambelkar17 <ruhiambelkar17@gmail.com>
2024-03-19 13:14:09 +0100

    created vue project

---
+++ vue-app/src/App.vue
@@ -0,0 +1,21 @@
+<script >
+import Home from './views/Home.vue'
+
+export default{
+  components:{
+   
+    Home
+  
+  }
+}
+</script>
+
+<template>
+  <div>
+    <Home></Home>
+   </div>
+</template>
+
+<style scoped>
+
+</style>
