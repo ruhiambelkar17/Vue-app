39a41c8b812860b1cdbd0fff5e55eb2f4da32a2c
ruhiambelkar17 <ruhiambelkar17@gmail.com>
2024-04-16 16:19:43 +0200

    integrated oauth2

--- vue-app/src/main.js
+++ vue-app/src/main.js
@@ -3,11 +3,15 @@ import './style.css'
 import App from './App.vue'
 import { createPinia } from 'pinia';
 import router from './router/Index';
+import vue3GoogleLogin from 'vue3-google-login';
 
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
+app.use(vue3GoogleLogin, {
+    clientId: '1059135692246-donflb298kffrdko5el2j7fsk4q31umc.apps.googleusercontent.com'
+  })
 
 
 app.mount("#app");
